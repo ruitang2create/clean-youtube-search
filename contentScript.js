7716c50a7e599a7990e5b349dc4874e9eb8ae02e
Rui Tang <ruitang@shair.co>
2023-07-28 02:40:21 -0700

    feat: remove meaningless background.js and other deprecated logic of messaging between utils and content script and the usage of localStorage

--- contentScript.js
+++ contentScript.js
@@ -3,11 +3,9 @@ const removedElements = [];
 function setRemoveForYou(isChecked) {
     if (isChecked) {
         // When checked, update 'hideForYou' to 'true' in localStorage
-        // localStorage.setItem('hideForYou', 'true');
         chrome.storage.sync.set({ "hideForYou": "true" });
     } else {
         // When unchecked, update 'hideForYou' to 'false' in localStorage
-        // localStorage.setItem('hideForYou', 'false');
         chrome.storage.sync.set({ "hideForYou": "false" });
     }
 }
@@ -23,23 +21,6 @@ chrome.storage.onChanged.addListener(({ hideForYou }) => {
         window.removeEventListener("scroll", removeForYouOnScroll);
     }
 })
-
-// // Listen for messages from utils.js
-// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-//     if (message) {
-//         console.log(`Remove for you? ${message.removeForYou}`);
-//         setRemoveForYou(message.removeForYou);
-//         if (message.removeForYou) {
-//             console.log("Recieved message of remove for you");
-//             removeForYou();
-//             sendResponse({ result: "Div elements removed." });
-//         } else {
-//             console.log("Recieved message of not to remove for you");
-//             location.reload();
-//             // Function to handle the 'For You' checkbox state change
-//         }
-//     }
-// });
 
 // Function to remove div elements
 function removeForYou() {
@@ -67,26 +48,19 @@ function removeForYou() {
     });
 }
 
-// async function removeForYouByLocalStorage() {
-//     // const hideForYou = localStorage.getItem('hideForYou');
-//     const hideForYou = await chrome.storage.sync.get("hideForYou");
-//     console.log(`HideForYou from chrome storage: ${JSON.stringify(hideForYou)}`);
-//     // chrome.runtime.sendMessage({ removeForYouByDefault: hideForYou });
-//     if (hideForYou === 'true') {
-//         // Send a message to the content script to trigger the removal logic
-//         // removeForYou();
-//         console.log("hideForYou is true, add the scroll listener");
-//         window.addEventListener("scroll", removeForYouOnScroll);
-//     } else {
-//         console.log("hideForYou is false, remove the scroll listener");
-//         window.removeEventListener("scroll", removeForYouOnScroll);
-//         if (removedElements.includes("for you")) {
-//             removedElements = removedElements.filter((e) => {
-//                 return e !== "for you";
-//             });
-//         }
-//     }
-// }
+async function removeForYouByLocalStorage() {
+    const { hideForYou } = await chrome.storage.sync.get("hideForYou");
+    console.log(`HideForYou from chrome storage: ${JSON.stringify(hideForYou)}`);
+    // chrome.runtime.sendMessage({ removeForYouByDefault: hideForYou });
+    if (hideForYou === 'true') {
+        // Send a message to the content script to trigger the removal logic
+        console.log("hideForYou is true, add the scroll listener");
+        window.addEventListener("scroll", removeForYouOnScroll);
+    } else {
+        console.log("hideForYou is false, remove the scroll listener");
+        window.removeEventListener("scroll", removeForYouOnScroll);
+    }
+}
 
 const removeForYouOnScroll = () => {
     if (removedElements.includes("for you")) {
@@ -97,15 +71,12 @@ const removeForYouOnScroll = () => {
     window.removeEventListener("scroll", removeForYouOnScroll);
 };
 
-// Add a listener for page scrolling
-// window.addEventListener("scroll", removeForYouOnScroll);
-
 // Check if the page has completely loaded
-// if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
-//     console.log("Page loaded completely");
-//     removeForYouByLocalStorage();
-// } else {
-//     console.log("DOM content loaded.");
-//     document.addEventListener("DOMContentLoaded", removeForYouByLocalStorage);
-// }
+if (document.readyState === "complete" || (document.readyState !== "loading" && !document.documentElement.doScroll)) {
+    console.log("Page loaded completely");
+    removeForYouByLocalStorage();
+} else {
+    console.log("DOM content loaded.");
+    document.addEventListener("DOMContentLoaded", removeForYouByLocalStorage);
+}
 console.log("content script running!");
--- utils.js
+++ utils.js
@@ -4,11 +4,6 @@ let pageStateInitiated = false;
 document.getElementById('for-you-checkbox').addEventListener('click', function () {
     if (pageStateInitiated) {
         const isChecked = this.checked;
-        // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        //     const activeTab = tabs[0];
-        //     // Send a message to the content script to trigger the removal logic
-        //     chrome.tabs.sendMessage(activeTab.id, { removeForYou: isChecked });
-        // });
         chrome.storage.sync.set({ "hideForYou": isChecked ? "true" : "false" });
     }
 });
@@ -24,13 +19,3 @@ const getInitialState = async () => {
 }
 
 getInitialState();
-// // Listen for messages from the background script
-// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-//     if (message && message.removeForYouByDefault !== undefined) {
-//         // Use the value of 'sayYes' received from the content script
-//         //   console.log("The value of 'sayYes' is:", message.sayYesValue);
-//         const forYouCheckbox = document.getElementById('for-you-checkbox');
-//         forYouCheckbox.checked = removeForYouByDefault === 'true';
-//         // You can use the value here as needed
-//     }
-// });
